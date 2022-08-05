f95309e6ee19138f5046fb06b1ce69501200c11a
Malegr0 <schoening.andreas@gmx.de>
2022-08-05 23:38:19 +0200

    added general handling of arguments

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-# This is a sample Python script.
-
-# Press Umschalt+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import argparse
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
+def evaluate_args():
+    parser = argparse.ArgumentParser()
+    # add arguments here
+    parser.add_argument("-t", action="store_true", help="test")
+
+    args = parser.parse_args()
+    if args.t:
+        print("test")
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    evaluate_args()
